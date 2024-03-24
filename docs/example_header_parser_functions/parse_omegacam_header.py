ef34712807266c0219c1f231c124167441ff231d
Frédéric Dux <duxfrederic@gmail.com>
2024-03-23 21:38:28 -0300

    simplifying paper branch; paper at root

--- docs/example_header_parser_functions/parse_omegacam_header.py
+++
@@ -1,8 +0,0 @@
-def parse_header(header):
-    from dateutil import parser
-    from astropy.time import Time
-    exptime = header['exptime']
-    gain = header['gain']
-    filter = header['filter']
-    time = Time(parser.parse(header['obstart']))
-    return {'exptime': exptime, 'gain': gain, 'filter': filter, 'mjd': time.mjd}
